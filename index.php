1ff68c2ded04fdc995c116eeb7a5238796113db3
appjedi <appjedi.net@gmail.com>
2023-12-01 07:38:59 -0800

    update repo
    update repo

--- index.php
+++ index.php
@@ -1,9 +1,18 @@
 <?php
 
 //var_dump($_POST);
-echo "Name: " . $_POST['name']."\n";
-foreach ($_POST['values'] as $row)
-    echo "Value: ". $row['val'] . "\n";
+//echo "Name: " . $_POST['fullName']."\n";
+$values =json_decode($_POST['values']);
+$size=count($values);
+//var_dump($values);
+
+//for ($i=0;$i<$size;$i++){
+foreach ($values as $row){
+    echo "$row->name <hr/>";
+    //var_dump($values[$i]->name);
+
+}
+    //echo "ID: ". $row['id'] . " - " . $row['name'] . "\n";
 
 
 
